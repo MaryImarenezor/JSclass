1b2a5cfb266c59dca8d8f5988a0a0acb1c10631b
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-09 15:45:37 +0000

    Changes to be committed: 	modified:   vehicleBaseClass.js

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,3 +1,26 @@
 class Vehicles {
-  
+  constructor(mileage, color, company){
+    this.color = color;
+    this.mileage = mileage;
+    this.company = company;
+  }
+  honda() {
+    if(this.company === "Honda") {
+        console.log("you have a honda car!");
+    }
+  }
+  badColor() {
+    if(this.color === "yellow" && "orange") {
+        console.log("those are not good colors for a car!");
+    }
+  }
+  speedster() {
+    if(this.mileage >= 90) {
+        console.log("Stop speeding >:[")
+    }
+  }
 }
+ let hondaCar = new Vehicles(120, "orange", "Honda");
+
+ console.log(hondaCar)
+ 
